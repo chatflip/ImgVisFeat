602be52420dcea06f4df8edda4555d40296565ad
chatflip <oo.chat.flip@gmail.com>
2024-08-24 22:22:58 +0400

    add normal test

--- tests/test_visualizer.py
+++ tests/test_visualizer.py
@@ -1,3 +1,5 @@
+import tempfile
+
 import pytest
 
 import imgvisfeat as ivf
@@ -17,14 +19,29 @@ def test_nonexistent_file() -> None:
     with pytest.raises(ValueError):
         vis.visualize(non_exists_file_path)
 
+
 def test_directory() -> None:
     vis = ivf.Visualizer()
     directory_path = "tests/resources/images"
     with pytest.raises(ValueError):
         vis.visualize(directory_path)
 
+
 def test_text() -> None:
     vis = ivf.Visualizer()
     text_path = get_test_text_path()
     with pytest.raises(ValueError):
         vis.visualize(text_path)
+
+
+def test_run_show() -> None:
+    vis = ivf.Visualizer()
+    image_path = get_test_image_path()
+    vis.visualize(image_path)
+
+
+def test_run_file() -> None:
+    vis = ivf.Visualizer()
+    image_path = get_test_image_path()
+    with tempfile.TemporaryDirectory() as temp_dir:
+        vis.visualize(image_path, dst_root=temp_dir)
