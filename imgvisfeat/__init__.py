9576dd041768c1398e4ee945b72e19c5c554771e
chatflip <oo.chat.flip@gmail.com>
2024-08-25 18:37:23 +0400

    export AbstractVisualizer

--- imgvisfeat/__init__.py
+++ imgvisfeat/__init__.py
@@ -1,21 +1,25 @@
 # noqa: D104
 from importlib.metadata import version
 
+from .AbstractVisualizer import AbstractVisualizer  # noqa: F401
 from .ColorChannelVisualizer import ColorChannelVisualizer  # noqa: F401
 from .ColorGradientVisualizer import ColorGradientVisualizer  # noqa: F401
 from .GrayGradientVisualizer import GrayGradientVisualizer  # noqa: F401
 from .HoGVisualizer import HoGVisualizer  # noqa: F401
+from .KeypointVisualizer import KeypointVisualizer  # noqa: F401
 from .LBPVisualizer import LBPVisualizer  # noqa: F401
 from .PowerSpectrumVisualizer import PowerSpectrumVisualizer  # noqa: F401
 from .Visualizer import Visualizer  # noqa: F401
 
 __all__ = [
     "Visualizer",
+    "AbstractVisualizer",
     "ColorChannelVisualizer",
     "ColorGradientVisualizer",
     "GrayGradientVisualizer",
     "HoGVisualizer",
     "LBPVisualizer",
+    "KeypointVisualizer",
     "PowerSpectrumVisualizer",
 ]
 
