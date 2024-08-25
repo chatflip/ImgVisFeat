32e77b5045dc412090917bd718ecc8450be41600
chatflip <oo.chat.flip@gmail.com>
2024-08-25 18:46:31 +0400

    merge GradientVisualizer

--- imgvisfeat/GradientVisualizer.py
+++ imgvisfeat/GradientVisualizer.py
@@ -41,3 +41,32 @@ class ColorGradientVisualizer(AbstractVisualizer):
         grad_y[:, :, 2] = cv2.filter2D(image[:, :, 2], -1, kernel_y)
         grad_xy[:, :, 2] = cv2.filter2D(image[:, :, 2], -1, kernel_xy)
         return GradientResult(grad_x, grad_y, grad_xy)
+
+
+class GrayGradientVisualizer(AbstractVisualizer):
+    """A class for computing the gradient of a grayscale image."""
+
+    def __init__(self) -> None:
+        """Initialize the GrayGradientVisualizer class."""
+        pass
+
+    def __call__(self, source: NDArray[np.uint8]) -> GradientResult:
+        """Compute the gradient of a grayscale image.
+
+        Args:
+            source (NDArray[np.uint8]): The source image.
+
+        Returns:
+            GradientResult: The gradient in the x, y, and xy directions.
+        """
+        if source.ndim != 2:
+            gray = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
+        else:
+            gray = source.copy()
+        kernel_x = np.array([[0, 0, 0], [-1, 0, 1], [0, 0, 0]], np.float32)
+        kernel_y = np.array([[0, 1, 0], [0, 0, 0], [0, -1, 0]], np.float32)
+        kernel_xy = np.array([[0, 1, 0], [-1, 0, 1], [0, -1, 0]], np.float32)
+        grad_x = cv2.filter2D(gray, -1, kernel_x).astype(np.uint8)
+        grad_y = cv2.filter2D(gray, -1, kernel_y).astype(np.uint8)
+        grad_xy = cv2.filter2D(gray, -1, kernel_xy).astype(np.uint8)
+        return GradientResult(grad_x, grad_y, grad_xy)
--- imgvisfeat/__init__.py
+++ imgvisfeat/__init__.py
@@ -3,8 +3,10 @@ from importlib.metadata import version
 
 from .AbstractVisualizer import AbstractVisualizer  # noqa: F401
 from .ColorChannelVisualizer import ColorChannelVisualizer  # noqa: F401
-from .ColorGradientVisualizer import ColorGradientVisualizer  # noqa: F401
-from .GrayGradientVisualizer import GrayGradientVisualizer  # noqa: F401
+from .GradientVisualizer import (  # noqa: F401
+    ColorGradientVisualizer,
+    GrayGradientVisualizer,
+)
 from .HoGVisualizer import HoGVisualizer  # noqa: F401
 from .KeypointVisualizer import KeypointVisualizer  # noqa: F401
 from .LBPVisualizer import LBPVisualizer  # noqa: F401
@@ -15,6 +17,7 @@ __all__ = [
     "Visualizer",
     "AbstractVisualizer",
     "ColorChannelVisualizer",
+    "GradientVisualizer",
     "ColorGradientVisualizer",
     "GrayGradientVisualizer",
     "HoGVisualizer",
